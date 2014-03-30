40153840ca0338b6bd1b07d6fe0b44325a283ea8
Joshua McFarland <mcfarljw@gmail.com>
2014-03-30 00:40:51 -0400

    Add more required methods and imports

--- src/android/Expansion.java
+++ src/android/Expansion.java
@@ -1,9 +1,32 @@
 package com.jernung.cordova.expansionfile;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
 
 public class Expansion extends CordovaPlugin {
-	
-	
-	
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false otherwise.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        return false;
+    }
 }
