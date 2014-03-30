4d42e11ad85eceba00f7572248530116a45c446a
Joshua McFarland <mcfarljw@gmail.com>
2014-03-30 00:52:36 -0400

    Add frame for js interface

---
+++ www/expansion.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+var cordova = require('cordova');
+var channel = require('cordova/channel');
+var utils = require('cordova/utils');
+
+function Expansion() {}
+
+var me = new Expansion();
+
+module.exports = me;
