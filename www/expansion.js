d9e839961f30f4a209d7ec4483b3a0757eec4c9e
Joshua McFarland <mcfarljw@gmail.com>
2014-04-02 19:52:22 -0400

    Simplify expansion file format

--- www/expansion.js
+++ www/expansion.js
@@ -3,8 +3,4 @@ var cordova = require('cordova');
 var channel = require('cordova/channel');
 var utils = require('cordova/utils');
 
-function Expansion() {}
-
-var me = new Expansion();
-
-module.exports = me;
+module.exports = {};
