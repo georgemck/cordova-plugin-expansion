101c64cdfd451a1b23394a8332e32fc7ebc5acae
Joshua McFarland <mcfarljw@gmail.com>
2014-04-09 13:03:01 +0800

    Pass empty arrays when using no arguments

--- www/expansion.js
+++ www/expansion.js
@@ -2,24 +2,24 @@ var exec = require('cordova/exec');
 
 module.exports = {
     getExpansionPaths: function(callback) {
-        exec(callback, null, 'Expansion', 'getExpansionPaths');
+        exec(callback, null, 'Expansion', 'getExpansionPaths', []);
     },
     getFile: function(filename, callback) {
         exec(callback, null, 'Expansion', 'getFile', [filename]);
     },
     isPlaying: function(callback) {
-        exec(callback, null, 'Expansion', 'isPlaying');
+        exec(callback, null, 'Expansion', 'isPlaying', []);
     },
     pauseMedia: function() {
-        exec(null, null, 'Expansion', 'pauseMedia');
+        exec(null, null, 'Expansion', 'pauseMedia', []);
     },
     setMedia: function(filename, callback) {
         exec(callback, null, 'Expansion', 'setMedia', [filename]);
     },
     startMedia: function() {
-        exec(null, null, 'Expansion', 'startMedia');
+        exec(null, null, 'Expansion', 'startMedia', []);
     },
     stopMedia: function() {
-        exec(null, null, 'Expansion', 'stopMedia');
+        exec(null, null, 'Expansion', 'stopMedia', []);
     }
 };
